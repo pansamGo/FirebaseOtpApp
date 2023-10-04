d8158da658393a3512f8abd2d9455e61dc3813f3
pansamGo <57519754+pansamGo@users.noreply.github.com>
2023-10-04 09:28:49 +0530

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@
  * @format
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -20,20 +20,35 @@ import { NavigationContainer } from '@react-navigation/native';
 import LoginScreen from './components/LoginScreen';
 import VerifyOtpScreen from './components/VerifyOtpScreen';
 import ProfileScreen from './components/ProfileScreen';
+import AppContext from './components/AppContext';
 
 
-function App() {
+function App () {
+
+  const [confirm, setConfirm] = useState(null);
+
+  const setConfirmObject = (value) => {
+    setConfirm(value);
+  }
+
+  const userSettings = {
+    confirmObj: confirm,
+    setConfirm,
+    setConfirmObject
+  }
 
   const Stack = createNativeStackNavigator();
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="LoginScreen">
-        <Stack.Screen name="LoginScreen" component={LoginScreen} options={{headerShown: false}}/>
-        <Stack.Screen name="VerifyOtpScreen" component={VerifyOtpScreen} options={{headerShown: false}}/>
-        <Stack.Screen name="ProfileScreen" component={ProfileScreen} options={{headerShown: false}}/>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AppContext.Provider value={userSettings}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="LoginScreen">
+          <Stack.Screen name="LoginScreen" component={LoginScreen} options={{headerShown: false}}/>
+          <Stack.Screen name="VerifyOtpScreen" component={VerifyOtpScreen} options={{headerShown: false}}/>
+          <Stack.Screen name="ProfileScreen" component={ProfileScreen} options={{headerShown: false}}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AppContext.Provider>
   );
 }
 
